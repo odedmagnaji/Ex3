e3630fc65912d33b71926242a2c97ccdd5823783
Localy <odedmagnaji@gmail.com>
2026-02-27 11:35:38 +0200

    Enhance GameDisplay to set background color and improve rendering of game board

--- src/mypacmangame/display/GameDisplay.java
+++ src/mypacmangame/display/GameDisplay.java
@@ -12,18 +12,20 @@ public class GameDisplay {
         int height = board.length;
         int width = board[0].length;
 
+        StdDraw.setPenColor(135, 206, 235);
+        StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.5);
+
+        StdDraw.setPenColor(30, 30, 200);
+
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 if (board[y][x] == 1) {
 
-                    // מתמטיקה של כיווץ: דוחסים את הקואורדינטות והגודל לטווח של 0 עד 1
                     double drawX = (x + 0.5) / width;
                     double drawY = (height - y - 0.5) / height;
-
                     double halfWidth = 0.5 / width;
                     double halfHeight = 0.5 / height;
 
-                    // מצייר מלבן מדויק ומכווץ
                     StdDraw.filledRectangle(drawX, drawY, halfWidth, halfHeight);
                 }
             }
